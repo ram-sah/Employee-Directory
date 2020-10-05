fb74a36b248ab8c076d4b4db8a3d511fd4a27a0c
Ram Sah <mohan2036@gmail.com>
2020-10-04 22:13:38 -0400

    Heroku setup

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 export default {
   // Gets all users
-  getUsers: function() {
-    return axios.get("https://randomuser.me/api/?results=200&nat=us");
+  getUsers: function () {
+    return axios.get("https://randomuser.me/api/?results=200&nat=us")
+      .then(response => {
+        console.log(response.data.results);
+        
+      });
   }
 };
